17953c504dcb86c41807d15d1deebf20162b3b8f
Naotarouu <darrencorpuz957@gmail.com>
2021-05-23 00:09:30 +0800

    Add project files.

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileTool,
+            this.toolHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(363, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileTool
+            // 
+            this.fileTool.Name = "fileTool";
+            this.fileTool.Size = new System.Drawing.Size(37, 20);
+            this.fileTool.Text = "File";
+            // 
+            // toolHelp
+            // 
+            this.toolHelp.Name = "toolHelp";
+            this.toolHelp.Size = new System.Drawing.Size(44, 20);
+            this.toolHelp.Text = "Help";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 88);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 88);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 48);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 88);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 88);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(120, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 88);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(225, 142);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 88);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(15, 236);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 88);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(120, 236);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 88);
+            this.button8.TabIndex = 8;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.clickButton);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(225, 236);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(99, 88);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.clickButton);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 350);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileTool;
+        private System.Windows.Forms.ToolStripMenuItem toolHelp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
