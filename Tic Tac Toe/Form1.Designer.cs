f47d65b4e0aa92a1d2ee22c92c864cc606de8a32
Naotarouu <darrencorpuz957@gmail.com>
2021-05-26 23:44:17 +0800

    Modified Screen, Menu, and Buttons.

--- Tic Tac Toe/Form1.Designer.cs
+++ Tic Tac Toe/Form1.Designer.cs
@@ -30,7 +30,7 @@ namespace Tic_Tac_Toe
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.new_gameTool = new System.Windows.Forms.ToolStripMenuItem();
             this.toolHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -48,29 +48,31 @@ namespace Tic_Tac_Toe
             // 
             this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileTool,
+            this.new_gameTool,
             this.toolHelp});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(363, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(342, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
-            // fileTool
+            // new_gameTool
             // 
-            this.fileTool.Name = "fileTool";
-            this.fileTool.Size = new System.Drawing.Size(37, 20);
-            this.fileTool.Text = "File";
+            this.new_gameTool.Name = "new_gameTool";
+            this.new_gameTool.Size = new System.Drawing.Size(77, 20);
+            this.new_gameTool.Text = "New Game";
             // 
             // toolHelp
             // 
             this.toolHelp.Name = "toolHelp";
             this.toolHelp.Size = new System.Drawing.Size(44, 20);
             this.toolHelp.Text = "Help";
+            this.toolHelp.Click += new System.EventHandler(this.click_helpTool);
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(15, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 88);
@@ -80,6 +82,7 @@ namespace Tic_Tac_Toe
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.Location = new System.Drawing.Point(120, 48);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(99, 88);
@@ -89,6 +92,7 @@ namespace Tic_Tac_Toe
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button3.Location = new System.Drawing.Point(225, 48);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(99, 88);
@@ -98,6 +102,7 @@ namespace Tic_Tac_Toe
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button4.Location = new System.Drawing.Point(15, 142);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(99, 88);
@@ -107,6 +112,7 @@ namespace Tic_Tac_Toe
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button5.Location = new System.Drawing.Point(120, 142);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(99, 88);
@@ -116,6 +122,7 @@ namespace Tic_Tac_Toe
             // 
             // button6
             // 
+            this.button6.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button6.Location = new System.Drawing.Point(225, 142);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(99, 88);
@@ -125,6 +132,7 @@ namespace Tic_Tac_Toe
             // 
             // button7
             // 
+            this.button7.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button7.Location = new System.Drawing.Point(15, 236);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(99, 88);
@@ -134,6 +142,7 @@ namespace Tic_Tac_Toe
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button8.Location = new System.Drawing.Point(120, 236);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(99, 88);
@@ -143,6 +152,7 @@ namespace Tic_Tac_Toe
             // 
             // button9
             // 
+            this.button9.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button9.Location = new System.Drawing.Point(225, 236);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(99, 88);
@@ -167,6 +177,7 @@ namespace Tic_Tac_Toe
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -178,7 +189,7 @@ namespace Tic_Tac_Toe
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileTool;
+        private System.Windows.Forms.ToolStripMenuItem new_gameTool;
         private System.Windows.Forms.ToolStripMenuItem toolHelp;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Form1.cs
@@ -12,6 +12,9 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+        int count = 0;
+        bool turn = true;
+
         public Form1()
         {
             InitializeComponent();
@@ -22,9 +25,65 @@ namespace Tic_Tac_Toe
 
         }
 
+        private void click_helpTool(object sender, EventArgs e)
+        {
+            MessageBox.Show("The first who can make diagonal or straight line wins.");
+        }
+
         private void clickButton(object sender, EventArgs e)
         {
+            Button button = (Button)sender;
+            button.Enabled = false;
+            turn = !turn;
+
+            if (turn)
+                button.Text = "O";
+            else
+                button.Text = "X";
+
+            player_winner();
+        }
+
+        private void player_winner()
+        { 
+            bool game_over = false;
+
+            if (!button1.Enabled && (button1.Text == button2.Text) && (button2.Text == button3.Text))
+                game_over = true;
+            else if (!button4.Enabled && (button4.Text == button5.Text) && (button5.Text == button6.Text))
+                game_over = true;
+            else if (!button7.Enabled && (button7.Text == button8.Text) && (button8.Text == button9.Text))
+                game_over = true;
+            else if (!button1.Enabled && (button1.Text == button5.Text) && (button5.Text == button9.Text))
+                game_over = true;
+            else if (!button3.Enabled && (button3.Text == button5.Text) && (button5.Text == button7.Text))
+                game_over = true;
+            else if (!button1.Enabled && (button1.Text == button4.Text) && (button4.Text == button7.Text))
+                game_over = true;
+            else if (!button2.Enabled && (button2.Text == button5.Text) && (button5.Text == button8.Text))
+                game_over = true;
+            else if (!button3.Enabled && (button3.Text == button6.Text) && (button6.Text == button9.Text))
+                game_over = true;
+
+            if (game_over)
+            {
+                String win = "";
+                if (turn)
+                    win = "O";
+                else
+                    win = "X";
+
+                MessageBox.Show("Game Over\n" + win + " wins");
+            }
+            else
+            {
+                
+            }
+            
+           
+            
 
         }
+
     }
 }
